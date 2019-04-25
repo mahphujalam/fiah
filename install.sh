07763988cc1c6792d03b2ec84c6417ecee365025
maruf <30905603+mahphujalam@users.noreply.github.com>
2019-04-25 17:43:36 +0530

    maruf
    maruf

---
+++ install.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+
+de='\033[1;30m'
+ef='\033[1;31m'
+a='\033[1;32m'
+b='\033[1;33m'
+c='\033[1;34m'
+d='\033[1;35m'
+e='\033[1;32m'
+f='\033[1;93m'
+g='\033[1;91m'
+h='\033[1;92m'
+i='\033[1;94m'
+j='\033[1;41m'
+k='\033[1;42m'
+l='\033[1;43m'
+m='\033[1;44m'
+n='\033[1;45m'
+o='\033[1;46m'
+p='\033[1;47m'
+q='\033[1;48m'
+r='\033[1;49m'
+s='\033[1;50m'
+
+
+echo " "
+chmod 777 *
+echo " "
+echo "$a Phising tool ka kuch imoptant tool $a"
+echo " "
+echo " "
+echo "$b Important tool install ho rha hai $b"
+apt update
+apt upgrade
+apt install python
+apt install python2
+apt install curl
+apt install w3m
+apt install wget
+apt install php
+apt install git
+apt install openssh
+apt instaL openssl-tool
+echo " "
+echo " "
+sleep 5
+echo "$c sara tool install ho gya hai $c"
+echo " "
+sleep 2
+echo "$d Ab Phising tool install ho rha hai $d"
+git clone http://github.com/thelinuxchoice/shellphish
+cd shellphish
+echo " "
+echo " "
+sleep 5
+echo "$e Successfully Install ho gya hai $e"
+echo " "
+echo " "
+echo "$f Ab ye start krne ke liye shortcut command install ho rha hai $f"
+echo " "
+echo " "
+cp fish /data/data/com.termux/files/usr/bin
+chmod 700 /data/data/com.termux/files/usr/bin/fish
+echo " "
+sleep 10
+echo "$g Successfull shortcut v install ho gya $g"
+echo " "
+echo "$h Ye type kar ke run kro ho jayega run $h"
+echo " "
+echo "$i fish $i"
